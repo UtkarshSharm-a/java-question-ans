4c442839c773ca3dc567962a37e53c1acbd78683
Utkarsh Sharma <utkarshsharma9919633720@gmail.com>
2025-11-03 01:35:06 +0530

    factorial of number

---
+++ question2.java
@@ -0,0 +1,16 @@
+// Factorial of a Number
+
+import java.util.Scanner;
+public class question2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n=sc.nextInt();
+    int factorial=1;
+    for(int i=1;i<=n;i++){
+        factorial=factorial*i;
+        } 
+      System.out.println(factorial);
+
+  }
+  
+}
